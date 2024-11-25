3430d50d75ede37f3c0fea7112e51fad79a7e427
sonms <smsms5676@naver.com>
2024-11-25 22:42:07 +0900

    feature/검색 데이터가 없을 경우 추가

--- app/src/main/java/com/purang/financial_ledger/screen/search/SearchScreen.kt
+++ app/src/main/java/com/purang/financial_ledger/screen/search/SearchScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -24,7 +25,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.purang.financial_ledger.room_db.FinancialEntity
@@ -39,7 +42,6 @@ fun SearchScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val allFinancialData by viewModel.financialListData.observeAsState(emptyList())
-    Log.e("searchText", searchText.toString())
     // 필터링된 데이터 - derivedStateOf
     //의존 상태가 변경되지 않으면 파생 상태를 재계산하지 않도록 보장합니다.
     //이는 큰 데이터 집합의 필터링이나 복잡한 계산 로직에 특히 유용합니다.
@@ -55,7 +57,6 @@ fun SearchScreen(
             }
         }
     }
-    Log.e("searchText", filteredData.toString())
 
     Column (
         modifier = Modifier
@@ -79,14 +80,23 @@ fun SearchScreen(
             }
         }
 
-        LazyColumn {
-            itemsIndexed(
-                items = filteredData
-            ) { _, item ->
-                HomeFinancialItem(item = item, onItemClick = {
-                    navController.navigate("edit_financial?type=edit&id=${item.id}")
-                })
+        if (filteredData.isNotEmpty()) {
+            LazyColumn {
+                itemsIndexed(
+                    items = filteredData
+                ) { _, item ->
+                    HomeFinancialItem(item = item, onItemClick = {
+                        navController.navigate("edit_financial?type=edit&id=${item.id}")
+                    })
+                }
             }
+        } else {
+            Text(
+                modifier = Modifier.fillMaxSize().padding(10.dp).align(Alignment.CenterHorizontally),
+                text = "검색된 데이터가 없습니다.",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
